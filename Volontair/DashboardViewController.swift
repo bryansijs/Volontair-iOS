ed7ba9e1864942d7e86285ffd570657184ed3723
bryansijs <bryansijs@gmail.com>
2016-03-11 11:43:56 +0100

    added icons and tabbarcontroller

---
+++ Volontair/DashboardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DashboardViewController.swift
+//  Volontair
+//
+//  Created by Bryan Sijs on 11-03-16.
+//  Copyright © 2016 Volontair. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
