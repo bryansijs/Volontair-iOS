dc5e7e3327a2acc8fde492ac820840c71acf636d
Bryan Sijs <bsijs1@avans.nl>
2016-04-14 20:49:34 +0200

    own cell

--- Volontair/ContactTableViewCell.swift
+++ Volontair/ContactTableViewCell.swift
@@ -3,8 +3,4 @@ import UIKit
 
 class ContactTableViewCell : UITableViewCell {
     // MARK: Properties
-    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var lastMessageLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
 }
