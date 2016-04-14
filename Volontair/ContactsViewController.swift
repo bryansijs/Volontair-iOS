1677d106a0d77236a6eab94114250f23f62bcdea
Bryan Sijs <bsijs1@avans.nl>
2016-04-14 12:17:25 +0200

    added contact view to tabbar

---
+++ Volontair/ContactsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.swift
+//  Volontair
+//
+//  Created by Bryan Sijs on 14-04-16.
+//  Copyright © 2016 Volontair. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+}
