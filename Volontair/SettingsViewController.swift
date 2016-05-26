9b1782b4fa20e7ff260a7c92a56df7ab219452ba
Bryan Sijs <bsijs1@avans.nl>
2016-05-26 10:35:51 +0200

    Settingsbar

--- Volontair/SettingsViewController.swift
+++ Volontair/SettingsViewController.swift
@@ -27,6 +27,9 @@ class SettingsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Settings"
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.BlackTranslucent
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 45, green: 183, blue: 207)
         radiusCell.userInteractionEnabled = true
         locationCell.userInteractionEnabled = true
         interestsCell.userInteractionEnabled = true;
@@ -46,7 +49,7 @@ class SettingsViewController: UITableViewController {
     
 
     override func viewWillAppear(animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        //self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
     override func viewWillDisappear(animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
