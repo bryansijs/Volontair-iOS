118adfb6a6d62116052339694ec58fb136243cc5
gnpiwano <gnpiwano@avans.nl>
2016-05-30 21:37:52 +0200

    standard image verandert naar nieuw plaatje van een uitroepteken wat alleen hiervoor gebruikt wordt.

--- Volontair/MapViewController.swift
+++ Volontair/MapViewController.swift
@@ -215,7 +215,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                 let image : UIImage = ApiConfig.categoryIconsWhite[markerAsUser.categorys![0].name]!
                 markerImage =  image.markerCircle(hexStringToUIColor(markerAsUser.categorys![0].colorHex))!
             } else {
-                let image = UIImage(named: "icon_events_white")!
+                let image = UIImage(named: "icon_category_default")!
                 markerImage =  image.markerCircle(hexStringToUIColor("#00bcd4"))!
             }
         }
