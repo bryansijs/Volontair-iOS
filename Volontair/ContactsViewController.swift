985fa97d2a2b176e867b3560cf6a59abd745fd82
gnpiwano <gnpiwano@avans.nl>
2016-05-31 13:26:51 +0200

    Item toegevoegd als er geen contacten in de lijst aanwezig zijn.
    Als er geen item in de lijst staat is contact opnemen niet mogelijk.

--- Volontair/ContactsViewController.swift
+++ Volontair/ContactsViewController.swift
@@ -103,17 +103,19 @@ class ContactsViewController : UIViewController, UITableViewDelegate, UITableVie
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let email = self.conversations[indexPath.row].listener!.username
-        let url = NSURL(string: "mailto:\(email)")
-        
-        if UIApplication.sharedApplication().canOpenURL(url!) {
-            UIApplication.sharedApplication().openURL(url!)
-        } else {
-            let alert = UIAlertController(title: NSLocalizedString("CONTACT_ERROR_TITLE", comment: "Comment"), message: NSLocalizedString("CONTACT_ERROR_MESSAGE", comment: "Comment"), preferredStyle: UIAlertControllerStyle.Alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in
-                self.navigationController?.popViewControllerAnimated(true)
-            }))
-            presentViewController(alert, animated: true, completion: nil)
+        if self.conversations[indexPath.row].listener != nil {
+            let email = self.conversations[indexPath.row].listener!.username
+            let url = NSURL(string: "mailto:\(email)")
+            
+            if UIApplication.sharedApplication().canOpenURL(url!) {
+                UIApplication.sharedApplication().openURL(url!)
+            } else {
+                let alert = UIAlertController(title: NSLocalizedString("CONTACT_ERROR_TITLE", comment: "Comment"), message: NSLocalizedString("CONTACT_ERROR_MESSAGE", comment: "Comment"), preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in
+                    self.navigationController?.popViewControllerAnimated(true)
+                }))
+                presentViewController(alert, animated: true, completion: nil)
+            }
         }
     }
     
