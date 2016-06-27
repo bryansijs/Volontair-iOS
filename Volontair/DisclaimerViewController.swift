03ca7c3f03a294b14735ff38405e48528ac58f6a
Bryan Sijs <bsijs1@avans.nl>
2016-06-27 22:35:56 +0200

    Added disclaimer screen

---
+++ Volontair/DisclaimerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DisclaimerViewController.swift
+//  Volontair
+//
+//  Created by Bryan Sijs on 27-06-16.
+//  Copyright © 2016 Volontair. All rights reserved.
+//
+
+import UIKit
+
+class DisclaimerViewController: UIViewController, ValidationProtocol {
+    func validate()-> Bool {
+        return true
+    }
+}
